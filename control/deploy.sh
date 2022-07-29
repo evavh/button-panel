b876f74e6be07ba7cd891663a1c2612055f99ef5
Eva van Houten <user@server.fake>
2022-07-29 12:13:42 +0200

    Make deploying dev set main version as fallback

--- control/deploy.sh
+++ control/deploy.sh
@@ -6,9 +6,23 @@ set -e
 #  - rust cross compile tool [cross] (cargo install cargo-cross)
 # please set the variables directly below
 
+BRANCH=$(git symbolic-ref --short HEAD)
+if [ $BRANCH = "dev" ]; then
+	echo "Deploying dev, with previously deployed main as fallback"
+	SUFFIX="_dev"
+	STOP_MAIN=";sudo systemctl stop button_panel.service;"
+elif [ $BRANCH = "main" ]; then
+	echo "Deploying main only"
+	SUFFIX=""
+	STOP_MAIN=""
+else
+	echo "Unknown branch $BRANCH, aborting"
+	exit 1
+fi
+
 SERVER_ADDR="pi"
 SERVER_USER="eva"
-SERVER_DIR="/home/$SERVER_USER/button_panel"
+SERVER_DIR="/home/$SERVER_USER/button_panel$SUFFIX"
 
 dir=debug
 if [ "$1" = "--release" ]; then
@@ -16,23 +30,23 @@ if [ "$1" = "--release" ]; then
 fi
 
 cross build --target=armv7-unknown-linux-gnueabihf $1
-rsync button_panel.service $SERVER_ADDR:/tmp/
+rsync button_panel$SUFFIX.service $SERVER_ADDR:/tmp/
 rsync -vh --progress \
   target/armv7-unknown-linux-gnueabihf/$dir/control \
   $SERVER_ADDR:/tmp/
 
 # sets up/updates the systemd service and places the binary
 cmds="
-sed -i \"s/<USER>/$SERVER_USER/g\" /tmp/button_panel.service
-sed -i \"s+<DIR>+$SERVER_DIR+g\" /tmp/button_panel.service
-sudo mv /tmp/button_panel.service /etc/systemd/system/
+sed -i \"s/<USER>/$SERVER_USER/g\" /tmp/button_panel$SUFFIX.service
+sed -i \"s+<DIR>+$SERVER_DIR+g\" /tmp/button_panel$SUFFIX.service
+sudo mv /tmp/button_panel$SUFFIX.service /etc/systemd/system/
 
 mkdir -p $SERVER_DIR
-mv /tmp/control $SERVER_DIR/button_panel
-chown $SERVER_USER:$SERVER_USER $SERVER_DIR/button_panel
+mv /tmp/control $SERVER_DIR/button_panel$SUFFIX
+chown $SERVER_USER:$SERVER_USER $SERVER_DIR/button_panel$SUFFIX
 
-sudo systemctl enable button_panel.service
-sudo systemctl restart button_panel.service
+sudo systemctl enable button_panel$SUFFIX.service$STOP_MAIN
+sudo systemctl restart button_panel$SUFFIX.service
 "
 
 ssh -t $SERVER_ADDR "$cmds"
